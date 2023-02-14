7d971afb51c829e2e0d6601be0fb458ed50281c0
Matthias Hulme <mh828@byu.edu>
2023-02-13 22:15:43 -0700

    Add project files.

---
+++ Mission06_mh828/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_mh828.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_mh828.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext _Context { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieContext contextName)
+        {
+            _logger = logger;
+            _Context = contextName;
+        }
+
+        // Index/Home Page Route
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // MovieSubmission Page Get Route
+        [HttpGet]
+        public IActionResult MovieSubmission()
+        {
+            return View();
+        }
+
+        // MovieSubmission Page Post Route
+        [HttpPost]
+        public IActionResult MovieSubmission(Movie m)
+        {
+            _Context.Add(m);
+            _Context.SaveChanges();
+
+            return View("Confirmation", m);
+        }
+
+        // MyPodcasts Route
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_mh828/Models/MovieContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_mh828.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options) {}
+
+        public DbSet<Movie> MovieResponses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Movie>().HasData(
+
+                // Creating Seed Movies
+                new Movie
+                {
+                    MovieId = 1,
+                    Category = "Action/Adventure",
+                    Title = "Tenet",
+                    Year = 2020,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13"
+                },
+                new Movie
+                {
+                    MovieId = 2,
+                    Category = "Comedy",
+                    Title = "Scott Pilgrim vs. the World",
+                    Year = 2010,
+                    Director = "Edgar Wright",
+                    Rating = "PG-13"
+                },
+                new Movie
+                {
+                    MovieId = 3,
+                    Category = "Action/Adventure",
+                    Title = "Star Wars Episode III: Revenge of the Sith",
+                    Year = 2005,
+                    Director = "George Lucas",
+                    Rating = "PG-13"
+                });
+        }
+    }
+}
